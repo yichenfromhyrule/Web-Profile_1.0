028cc8c832c6fd064581db17f83ed6b44266f081
yichenfromhyrule <jambako1995@gmail.com>
2020-09-29 09:40:54 -0400

    adding files

---
+++ root/js/style.js
@@ -0,0 +1,23 @@
+$(document).scroll(function(){
+	var y = $(this).scrollTop();
+	if(y>800){
+		$('.row_b').fadeIn();
+	} else{
+		$('.row_b').fadeOut();
+	}
+});
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
